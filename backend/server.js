d497658f59260f8083129f6f55ac1fe75a62f68f
Thewellfire <info.wellfire@gmail.com>
2025-12-15 21:42:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(cors({
         "http://localhost:5176",
         "http://localhost:5180",
         "http://localhost:3000",
-        process.env.FRONTEND_URL || "https://wellfire-frontend-oa5j.onrender.com",
+        process.env.FRONTEND_URL || "https://www.thewellfire.com/",
         process.env.ADMIN_URL || "https://wellfire-new2.onrender.com",
         "https://wellfire-frontend-oa5j.onrender.com",
         "https://wellfire-new2.onrender.com"
